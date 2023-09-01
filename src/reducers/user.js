dd0ccb821472bd8bd76838f72f52016bd886a34b
Đặng Trương Minh Trí <87639905+seeyounxttime@users.noreply.github.com>
2023-09-01 15:38:34 +0700

    initial commit

---
+++ src/reducers/user.js
@@ -0,0 +1,17 @@
+const user = {
+  avatarUrl:
+    "https://res.cloudinary.com/dpmimldgh/image/upload/v1643205032/18d9e1307018dbc76750ca7d5124fccd_hcdpid.jpg",
+  username: "minhtri",
+  password: "123123",
+  socials: {
+    twitter: "https://twitter.com/",
+    instagram: "https://www.instagram.com/",
+  },
+  joinDate: "August 2023",
+};
+
+const userReducer = () => {
+  return user;
+};
+
+export default userReducer;
